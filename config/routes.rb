4afe6b7fc8700bea9e15ae1b67720a699bcb6f93
shan <corallishan@gmail.com>
2013-09-03 22:12:18 -0400

    Created Post resource and updated index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Bloccit::Application.routes.draw do
 
+  get "posts/index"
+
+  get "posts/show"
+
+  get "posts/new"
+
+  get "posts/edit"
+
   resources :posts
 
   match "about" => 'welcome#about', via: :get
