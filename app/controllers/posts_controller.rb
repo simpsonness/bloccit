95af0bb4704a9e6b313e4ce918b95c305a9f6d86
shan <corallishan@gmail.com>
2013-09-13 22:14:13 -0400

    Implemented Cancan and basic roles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,10 +10,12 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    authorize! :create, Post, message: "You need to be a member to create a new post."
   end
 
   def create
     @post = current_user.posts.build(params[:post])
+    authorize! :create, @post, message: "You need to be signed up to do that."
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
@@ -25,16 +27,18 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+    authorize! :edit, @post, message: "You need to own the post to edit it."
   end
 
   def update
     @post = Post.find(params[:id])
+     authorize! :update, @post, message: "You need to own the post to edit it."
     if @post.update_attributes(params[:post])
       flash[:notice] = "Post was updated."
       redirect_to @post
     else
       flash[:error] = "There was an error saving the post. Please try again."
-      render :new
+      render :edit
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,6 @@ Bloccit::Application.routes.draw do
 
   devise_for :users
 
-  get "posts/index"
-
-  get "posts/show"
-
-  get "posts/new"
-
-  get "posts/edit"
-
   resources :posts
 
   match "about" => 'welcome#about', via: :get
