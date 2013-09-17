217164870ed9543724a15a64acc520eb87efb298
shan <corallishan@gmail.com>
2013-09-17 17:37:07 -0400

    minor changes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Bloccit::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   # http://bloccit.herokuapp.com
-  config.action_mailer.default_url_options = { :host => 'bloccit.herokuapp.com' }  
+  config.action_mailer.default_url_options = { :host => 'yourname-bloccit.herokuapp.com' }  
 end
