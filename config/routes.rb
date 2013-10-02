d6b070d9a21c8140e4c7ebe98b05d06b137bce89
shan <corallishan@gmail.com>
2013-10-01 22:45:25 -0400

    check the code

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,20 @@ Bloccit::Application.routes.draw do
 
   get "comments/create"
 
-devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
+
+  
   resources :topics do 
     resources :posts, except: [:index] do
       resources :comments, only: [:create, :destroy] 
-        resources :favorites, only: [:create, :destroy]
-          resources :users, only:[:show] #create a route for users#show
+      resources :favorites, only: [:create, :destroy]
+
       match '/up-vote', to: 'votes#up_vote', as: :up_vote
       match '/down-vote', to: 'votes#down_vote', as: :down_vote
     end
   end
+
+ resources :users, only:[:show] #create a route for users#show
 
   match "about" => 'welcome#about', via: :get
 
